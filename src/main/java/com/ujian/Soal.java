fdf9646854a6c7b5e366a6b0aec6077bac699b38
fahrizalnuansa <fnuansa@gmail.com>
2014-05-21 15:37:22 +0700

    commit pertama ujian rkppl

---
+++ src/main/java/com/ujian/Soal.java
@@ -0,0 +1,27 @@
+package com.ujian;
+
+public class Soal {
+
+	public int pangkat(int bil, int pangkat) {
+		int hasil = 0;
+		int n = 1;
+		while (n <= pangkat) {
+			n++;
+			hasil *= bil;
+		}
+		return hasil;
+	}
+
+	public int faktorial(int bil) {
+		int hasil = 1;
+		for (int i = 0; i < bil; i++) {
+			hasil *= i;
+		}
+		return hasil;
+	}
+
+	public int perkalian(int bilA, int bilB) {
+		return bilA * bilB;
+	}
+
+}
